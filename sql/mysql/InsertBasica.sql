d343b5c8b6e521cf9088721cb6987b6bceef9a8e
JoseRamonBravo <ramonbravo1000@gmail.com>
2024-04-04 12:52:58 +0200

    Cambios en los scripts

--- sql/mysql/InsertBasica.sql
+++ sql/mysql/InsertBasica.sql
@@ -3,8 +3,8 @@ USE DNCONTROL;
 START TRANSACTION;
 
 -- Insercion usuarios
-INSERT INTO USUARIO (EMAIL, NOMBRE_USUARIO, PASSWD) VALUES
-('admin@gmail.com', 'admin', '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918');
+INSERT INTO USUARIO (EMAIL, NOMBRE_USUARIO, PASSWD, DM) VALUES
+('admin@gmail.com', 'admin', '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918', true);
 -- Fin usuarios
 
 COMMIT;
