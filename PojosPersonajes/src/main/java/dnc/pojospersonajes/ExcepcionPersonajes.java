ab503525576883ab287b5407f20da7116e029745
JoseRamonBravo <ramonbravo1000@gmail.com>
2026-04-10 16:50:45 +0200

    Modiicar la clase excepcion para que contenga el enum de errores

--- PojosPersonajes/src/main/java/dnc/pojospersonajes/ExcepcionPersonajes.java
+++ PojosPersonajes/src/main/java/dnc/pojospersonajes/ExcepcionPersonajes.java
@@ -1,6 +1,7 @@
 package dnc.pojospersonajes;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 public class ExcepcionPersonajes extends Exception implements Serializable {
 
@@ -9,27 +10,30 @@ public class ExcepcionPersonajes extends Exception implements Serializable {
     private String mensajeErrorAdmin;
     private String sentenciaSql;
     private Integer codigoErrorBd;
+    private ErrorUsuario error;
 
     public ExcepcionPersonajes() {
         mensajeUsuario = "";
         mensajeErrorAdmin = "";
         sentenciaSql = "";
         codigoErrorBd = null;
+        error = ErrorUsuario.DEFAULT;
     }
 
-    public ExcepcionPersonajes(String mensajeUsuario, String mensajeErrorBd, String sentenciaSql, Integer codigoErrorBd) {
+    public ExcepcionPersonajes(String mensajeUsuario, String mensajeErrorAdmin, String sentenciaSql, Integer codigoErrorBd, ErrorUsuario error) {
         this.mensajeUsuario = mensajeUsuario;
-        this.mensajeErrorAdmin = mensajeErrorBd;
+        this.mensajeErrorAdmin = mensajeErrorAdmin;
         this.sentenciaSql = sentenciaSql;
         this.codigoErrorBd = codigoErrorBd;
+        this.error = error;
     }
 
-    public Integer getCodigoErrorBd() {
-        return codigoErrorBd;
-    }
-
-    public void setCodigoErrorBd(Integer codigoErrorBd) {
+    public ExcepcionPersonajes(String mensajeUsuario, String mensajeErrorAdmin, String sentenciaSql, Integer codigoErrorBd) {
+        this.mensajeUsuario = mensajeUsuario;
+        this.mensajeErrorAdmin = mensajeErrorAdmin;
+        this.sentenciaSql = sentenciaSql;
         this.codigoErrorBd = codigoErrorBd;
+        error = ErrorUsuario.DEFAULT;
     }
 
     public String getMensajeUsuario() {
@@ -56,9 +60,51 @@ public class ExcepcionPersonajes extends Exception implements Serializable {
         this.sentenciaSql = sentenciaSql;
     }
 
+    public Integer getCodigoErrorBd() {
+        return codigoErrorBd;
+    }
+
+    public void setCodigoErrorBd(Integer codigoErrorBd) {
+        this.codigoErrorBd = codigoErrorBd;
+    }
+
+    public ErrorUsuario getError() {
+        return error;
+    }
+
+    public void setError(ErrorUsuario error) {
+        this.error = error;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final ExcepcionPersonajes other = (ExcepcionPersonajes) obj;
+        if (!Objects.equals(this.mensajeUsuario, other.mensajeUsuario)) {
+            return false;
+        }
+        if (!Objects.equals(this.mensajeErrorAdmin, other.mensajeErrorAdmin)) {
+            return false;
+        }
+        if (!Objects.equals(this.sentenciaSql, other.sentenciaSql)) {
+            return false;
+        }
+        if (!Objects.equals(this.codigoErrorBd, other.codigoErrorBd)) {
+            return false;
+        }
+        return this.error == other.error;
+    }
+
     @Override
     public String toString() {
         return codigoErrorBd + "\t" + mensajeErrorAdmin + "\n\t" + sentenciaSql;
     }
-
 }
